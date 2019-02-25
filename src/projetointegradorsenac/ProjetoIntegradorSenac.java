598bf882d7b8d5878a1bb257f442fe69f272b4bb
Daniel de Assis <danieldeassis@gmail.com>
2019-02-25 09:12:08 -0300

    Classe principal alterada com mensagem de exemplo.

--- src/projetointegradorsenac/ProjetoIntegradorSenac.java
+++ src/projetointegradorsenac/ProjetoIntegradorSenac.java
@@ -15,7 +15,7 @@ public class ProjetoIntegradorSenac {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Estudando o git e github.");
     }
     
 }
